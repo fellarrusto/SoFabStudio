e17c5ed7a581825cca9f10e0aa02794f1cd6a4bc
fabsow <fabry92@msn.com>
2024-02-25 17:58:40 +0100

    robots, sitemap, metatag ecc.

--- views/project.php
+++ views/project.php
@@ -24,18 +24,17 @@ $projectdata = new ProjectData($url, $token, $projectname);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title><?php echo $projectdata->getData()["nome"] ?> - So Fab Studio</title>
-    <link rel="canonical" href="https://www.sofabstudio.com/project">
-    <meta property="og:url" content="https://www.sofabstudio.com/project">
+    <link rel="canonical" href="https://www.sofabstudio.com/project?project=<?= urlencode($projectName) ?>">
+    <meta property="og:url" content="https://www.sofabstudio.com/project?project=<?= urlencode($projectName) ?>">
     <meta http-equiv="Strict-Transport-Security" content="max-age=31536000">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="index,follow">
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:image" content="<?php echo htmlspecialchars($projectdata->getData()["fotoInterna"]["url"], ENT_QUOTES, 'UTF-8'); ?>">
-    <meta name="description" content="Sono Fabrizio, creo contenuti, gestisco canali social
-e campagne pubblicitarie per migliorare la comunicazione digitale di aziende e professionisti.">
-    <meta property="og:description" content="Con me aziende e professionisti migliorano la loro comunicazione e presenza online. Branding, Comunicazione e Social Media.">
+    <meta name="description" content="<?= htmlspecialchars($descrizioneBreve) ?>
+    <meta property="og:description" content="<?= htmlspecialchars($descrizioneBreve, ENT_QUOTES, 'UTF-8') ?>">
     <meta property="og:type" content="website">
-    <meta property="og:title" content="So Fab Studio - Comunicazione & Marketing">
+    <meta property="og:title" content="<?php echo $projectdata->getData()["nome"] ?> - So Fab Studio">
     <meta property="og:image" content="<?php echo htmlspecialchars($projectdata->getData()["fotoInterna"]["url"], ENT_QUOTES, 'UTF-8'); ?>">
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
@@ -43,6 +42,15 @@ e campagne pubblicitarie per migliorare la comunicazione digitale di aziende e p
     <meta property="og:image" content="<?php echo htmlspecialchars($projectdata->getData()["fotoInterna"]["url"], ENT_QUOTES, 'UTF-8'); ?>">
     <meta property="og:image:width" content="400" />
     <meta property="og:image:height" content="400" />
+
+    <script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "CreativeWork",
+  "name": "<?php echo $projectdata->getData()["nome"] ?> - So Fab Studio",
+  "url": "https://www.sofabstudio.com/project?project=<?= urlencode($projectName) ?>"
+ }
+</script>
 
     <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="assets/img/180x180.png">
     <link rel="icon" type="image/png" sizes="16x16" href="assets/img/16X16.png">
@@ -131,7 +139,7 @@ e campagne pubblicitarie per migliorare la comunicazione digitale di aziende e p
         <div class="container position-relative" id="contatti">
             <div class="row mb-5" style="color: var(--bs-emphasis-color);">
                 <div class="col-md-8 col-xl-6 text-center mx-auto">
-                    <h2>contattami!</h2>
+                    <h2>Ti è piaciuto? Contattami!</h2>
                     <p class="w-lg-50" style="font-family: 'Space Grotesk', sans-serif;">puoi trovarmi via telefono o tramite mail :)</p>
                 </div>
             </div>
@@ -139,12 +147,12 @@ e campagne pubblicitarie per migliorare la comunicazione digitale di aziende e p
                 <div class="col-md-6 col-lg-4 col-xl-4">
                     <div class="d-flex flex-column justify-content-center align-items-start h-100">
                         <div class="d-flex align-items-center p-3">
-                            <div class="bs-icon-md bs-icon-rounded bs-icon-primary d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block bs-icon" style="background: transparent;"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-phone-fill" style="color: var(--bs-emphasis-color);" href="tel:00393395432769">
+                            <div class="bs-icon-md bs-icon-rounded bs-icon-primary d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block bs-icon" style="background: transparent;"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-phone-fill" style="color: var(--bs-emphasis-color);" href="tel:00393792419000">
                                     <path d="M3 2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2zm6 11a1 1 0 1 0-2 0 1 1 0 0 0 2 0"></path>
                                 </svg></div>
                             <div class="px-2">
                                 <h6 class="mb-0" style="font-family: 'Space Grotesk', sans-serif;"><strong>Telefono</strong></h6>
-                                <p class="mb-0" style="font-family: 'Space Grotesk', sans-serif;"><a href="tel:00393395432769" style="text-decoration:none;"><span style="color: rgb(0, 0, 0);">+39 3395432769</span></a></p>
+                                <p class="mb-0" style="font-family: 'Space Grotesk', sans-serif;"><a href="tel:00393792419000" style="text-decoration:none;"><span style="color: rgb(0, 0, 0);">+39 3792419000</span></a></p>
                             </div>
                         </div>
                         <div class="d-flex align-items-center p-3">
@@ -184,7 +192,7 @@ e campagne pubblicitarie per migliorare la comunicazione digitale di aziende e p
                                 <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"></path>
                             </svg></a></li>
                 </ul>
-                <p class="mb-0" style="font-family: 'Space Grotesk', sans-serif;">Fabrizio Somma - So Fab Studio | <a href="mailto:info@sofabstudio.com" style="text-decoration:none;"><span style="color: rgb(0, 0, 0);">info@sofabstudio.com</span></a>&nbsp;|&nbsp;<a href="tel:00393395432769" style="text-decoration:none;"><span style="color: rgb(0, 0, 0);">+39 3395432769</span></a></p>
+                <p class="mb-0" style="font-family: 'Space Grotesk', sans-serif;">Fabrizio Somma - So Fab Studio | <a href="mailto:info@sofabstudio.com" style="text-decoration:none;"><span style="color: rgb(0, 0, 0);">info@sofabstudio.com</span></a>&nbsp;|&nbsp;<a href="tel:00393792419000" style="text-decoration:none;"><span style="color: rgb(0, 0, 0);">+39 3792419000</span></a></p>
                 <p class="mb-0" style="font-family: 'Space Grotesk', sans-serif;margin-top: 20px;font-size: 10px;">Illustrazioni by&nbsp;<a href="https://www.instagram.com/theycallme_tommy/" style="text-decoration:underline #000000;"><span style="color: rgb(0, 0, 0);">@theycallme_tommy</span></a></p>
             </div>
         </footer>
